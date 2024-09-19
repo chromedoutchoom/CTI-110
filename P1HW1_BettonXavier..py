6f0dfb74956aa02351ffac4b652b17ab7b58d90e
chromedoutchoom <bettonx9310@student.faytechcc.edu>
2024-09-19 11:46:36 -0400

    Add files via upload

---
+++ P1HW1_BettonXavier..py
@@ -0,0 +1,38 @@
+# Xavier Betton
+# 9/17/24
+# P1HW1
+# Input and Output with mathematical expression
+
+print ("-----Calculating Exponents-----")
+print()
+print()
+print()
+
+# Get base value (as an integer) from the user
+base_value = int(input("Enter an integer as a base value: "))
+
+# Get exponent value from user
+exponent = int(input("Enter an inteher as the exponent: "))
+
+# Raise the base_value to the exponent
+value = base_value ** exponent
+
+print()
+print()
+
+# Display output to the user
+print(base_value, "raised to the power of", exponent, "is", value, "!!")
+
+print()
+print()
+print("------Addition and Subtraction---------")
+print()
+print()
+start_num = int(input("Enter a starting integer: "))
+add_num = int(input("Enter an integer to add: "))
+sub_num = int(input("Enter an integer to subtract: "))
+print()
+print()
+print(start_num, "+", add_num, "-", sub_num, "is equal to", start_num + add_num - sub_num) 
+
+
