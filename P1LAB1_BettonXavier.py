9ec1388a29fe73527a337b5319b27077487ea071
chromedoutchoom <bettonx9310@student.faytechcc.edu>
2024-09-19 10:38:02 -0400

    Add files via upload

---
+++ P1LAB1_BettonXavier.py
@@ -0,0 +1,14 @@
+# Xavier Betton
+# 9/17/24
+# P1LAB1
+# Get Input from the user and display output
+
+# Get first name from user
+first_name = input ("Enter your first name: ")
+
+# Get last name from user
+last_name = input ("Enter your last name: ")
+
+# Display info to user
+print ("Hello, ", first_name, last_name + "! Welcome to CTI-110")
+ 
